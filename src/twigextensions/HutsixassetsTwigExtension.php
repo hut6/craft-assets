bb0d41ff93fd6152d00ec795176de1a6d67eeadb
Luke <lukemcdonnl@gmail.com>
2020-06-25 14:55:29 +0930

    Bugfix

--- src/twigextensions/HutsixassetsTwigExtension.php
+++ src/twigextensions/HutsixassetsTwigExtension.php
@@ -146,7 +146,7 @@ class HutsixassetsTwigExtension extends \Twig_Extension
      */
     private function getManifestPath()
     {
-        return getenv('MANIFEST_PATH') ?: $this->toAbsolutePath('/assets/manifest.json');
+        return $this->toAbsolutePath((getenv('MANIFEST_PATH') ?: '/assets/manifest.json'));
     }
 
     /**
